8aaaed51cd026c2ec35d47a51067d79d6dc06cb6
Derek Wene <derek.wene@gmail.com>
2020-09-18 11:16:50 -0500

    add more logging to statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,13 @@ context="$INPUT_CONTEXT"
 url="$INPUT_URL"
 command="$INPUT_COMMAND"
 
-
 echo $token
 echo $context
 echo $url
 echo $command
 
-rancher login --token $token $url && \
-rancher context switch $context && \
-rancher $command
+echo rancher login --token $token $url
+echo rancher context switch $context
+echo rancher $command
+
+rancher login --token $token $url && rancher context switch $context && rancher $command
