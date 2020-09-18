c5e5eda8fe056453e4434d5dc92aa1ef84b20b9a
Derek Wene <derek.wene@gmail.com>
2020-09-18 10:39:44 -0500

    try this instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,14 @@
 token="$INPUT_TOKEN"
 context="$INPUT_CONTEXT"
 url="$INPUT_URL"
+command="$INPUT_COMMAND"
 
-rancher login --token $token $url
-rancher context switch $context
-rancher $*
+
+echo $token
+echo $context
+echo $url
+echo $command
+
+rancher login --token $token $url && \
+rancher context switch $context && \
+rancher $command
