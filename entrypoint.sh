ba2c4839b784dddd0154f24e9b1dd4d4341061fd
Derek Wene <derek.wene@gmail.com>
2020-09-18 11:42:00 -0500

    fix entrypoint with rancher cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,8 @@ context="$INPUT_CONTEXT"
 url="$INPUT_URL"
 command="$INPUT_COMMAND"
 
-echo $token
-echo $context
-echo $url
-echo $command
-echo $GITHUB_SHA
-
-echo rancher login --token $token $url
-echo rancher context switch $context
+echo "running the following commands:"
+echo rancher login --token $token $url --context $context
 echo rancher $command
 
-rancher login --token $token $url && rancher context switch $context && rancher $command
+rancher login --token $token $url --context $context && rancher $command
